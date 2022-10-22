02e113aaba6e07bbb30d9955a22e8f3c1455fd1d
austin-bagwell <austinbagwell.dev@gmail.com>
2022-10-22 12:50:06 -0400

    cleaned comments, added todos

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
 "use strict";
-// For a customer with a known average time in transit and a known due date,
-// Best ship date = the Date value of:
-//   availableShipDays - avgTimeInTransit
-//   where availableShipDays = dueDate - today - weekend days
+// TODO create dummy data to test the cust parameter of main function
 
-// FIXME not correctly calculating 'ship in x days' - need to think more mathematically about that bit
+// TODO getting inputs from DOM
 
 // TODO correctly formatting dates to pass into all Date functions?
 // works with a string formatted "MM/DD/YYYY"
 // need to validate that all inputs passed from forms match that input
 
-// TODO how to turn # of days into an actual Date value
-// aka go from "ship in 3 days" to "ship on 10/31/22"
-
 // DOM ELEMENTS
 const screen = document.querySelector("#screen");
 
+//
 // HELPER FUNCTIONS
-function numDaysBetweenDates(start, end) {
-  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-  const utc1 = Date.UTC(start.getFullYear(), start.getMonth(), start.getDate());
-  const utc2 = Date.UTC(end.getFullYear(), end.getMonth(), end.getDate());
-
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-}
-
-function countWeekendDays(startDate, endDate) {
-  let count = 0;
-
-  while (startDate < endDate) {
-    startDate.setDate(startDate.getDate() + 1);
-    if (startDate.getDay() === 0 || startDate.getDay() === 6) {
-      count++;
-    }
-  }
-  return count;
-}
+//
 
 Date.prototype.subtractDays = function (days) {
   const date = new Date(this.valueOf());
@@ -43,33 +20,28 @@ Date.prototype.subtractDays = function (days) {
   return date;
 };
 
+//
+//
 // MAIN FUNCTION
-// best = dueDate - transitTime - any num of weekend days
-// TODO add proper return value
-// TODO add test cases - seems to work appropriately now
+//
+//
 
 function findBestShipDate(cust, due) {
-  const transitTime = 3;
+  const transitTime = 7;
   const dueDate = new Date(due);
   let bestShipDate = new Date(due);
   let daysInTransit = 0;
   let weekendDays = 0;
   let dayToCheck = new Date(due);
-  let calendarDays = 0;
-
   while (daysInTransit < transitTime) {
     dayToCheck = dueDate.subtractDays(daysInTransit + 1);
-    // console.log(
-    //   `Checking day: ${dayToCheck.getMonth() + 1}/${dayToCheck.getDate()}`
-    // );
     if (dayToCheck.getDay() === 0 || dayToCheck.getDay() === 6) {
-      console.log(`^^ that's a weekend day`);
       weekendDays++;
     }
     daysInTransit++;
   }
 
-  calendarDays = daysInTransit + weekendDays;
+  const calendarDays = daysInTransit + weekendDays;
   bestShipDate = bestShipDate.subtractDays(calendarDays);
 
   if (bestShipDate.getDay() === 0) {
