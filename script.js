7201d32ff3d36ceadd925febeaf58ad756c40f13
austin-bagwell <austinbagwell.dev@gmail.com>
2022-10-27 20:58:32 -0400

    put bestShipDate() into module

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { findBestShipDate } from "./modules/bestShipDate.js";
+
 // DOM
 const selectConsignee = document.querySelector("#select-consignee");
 const btnSubmit = document.querySelector("#submit");
@@ -58,47 +60,3 @@ btnSubmit.addEventListener("click", () => {
     );
   }
 });
-
-//
-// HELPER FUNCTIONS
-//
-Date.prototype.subtractDays = function (days) {
-  const date = new Date(this.valueOf());
-  date.setDate(date.getUTCDate() - days);
-  return date;
-};
-
-//
-//
-// MAIN FUNCTION
-//
-//
-function findBestShipDate(timeInTransit, due) {
-  const transitTime = timeInTransit;
-  const dueDate = new Date(`${due}T00:00`);
-  let bestShipDate = new Date(`${due}T00:00`);
-  let daysInTransit = 0;
-  let weekendDays = 0;
-
-  while (daysInTransit < transitTime) {
-    let dayToCheck = new Date(due);
-    dayToCheck = dueDate.subtractDays(daysInTransit + 1);
-    if (dayToCheck.getDay() === 0 || dayToCheck.getDay() === 6) {
-      weekendDays++;
-    }
-    daysInTransit++;
-  }
-
-  const calDaysInTransit = daysInTransit + weekendDays;
-  bestShipDate = bestShipDate.subtractDays(calDaysInTransit);
-
-  if ([0, 6].includes(bestShipDate.getDay())) {
-    bestShipDate = bestShipDate.subtractDays(1);
-  }
-
-  const message = `Best day to ship: ${
-    bestShipDate.getMonth() + 1
-  }/${bestShipDate.getDate()}`;
-
-  return message;
-}
