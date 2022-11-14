b565d008e4678e335ae16cbe1d36916830974a66
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-13 20:26:04 -0500

    refactor folder structure

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 "use strict";
 
 const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send(`app.get('/') from app.js`);
+// });
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
