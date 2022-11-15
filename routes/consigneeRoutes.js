b8d7203398d0742fefd9e8835834e5f384ccf68f
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-14 19:10:52 -0500

    starchilds help setting up API routes

---
+++ routes/consigneeRoutes.js
@@ -0,0 +1,34 @@
+"use strict";
+// import { Router } from "express";
+const express = require("express");
+const router = express.Router();
+
+// TODO
+const dummyJSON = {
+  consignees: [
+    "Kroger",
+    "KeHE - Chino",
+    "Wegmans",
+    "UNFI - Dayville",
+    "UNFI - Moreno Valley",
+  ],
+  avgTransitTimes: [3, 2, 3, 1, 7],
+  shipFrom: ["DUR", "SF", "DUR", "DUR", "SF"],
+};
+
+/**
+ * Router to define all consignee routes
+ * @param {Router} router
+ *
+ */
+// export const consigneeRoutes = (router) => {
+// defining the route
+// localhost:3000/get-consignees
+router.get("/get-consignees", (req, res) => {
+  res.json(dummyJSON);
+  // also go to MySQL and execute a query
+  // let data = getDataFromSQL()
+  // res.json(data)
+});
+module.exports = router;
+// };
