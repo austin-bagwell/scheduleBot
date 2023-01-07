6440e65c99207136f109e01d2806ff49b4df0be6
austin-bagwell <austinbagwell.dev@gmail.com>
2023-01-06 19:21:39 -0500

    removed fluff - comments, unused routes

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 "use strict";
 
 const consigneeRoutes = require("./backend/routes/consigneeRoutes");
-const proNumbers = require("./backend/routes/proNumbers");
 
 const express = require("express");
 const app = express();
 
 const port = process.env.port || 3000;
-// router.use((req, res, next) => {
-//   // middleware specific to this router
-//   // if (req) {do auth or whatever}
-//   console.log(`if router is used, you'll see me!`);
-//   next();
-// });
 
-// runs fn to set up routes for consignee specifically
-// consigneeRoutes(router);
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -24,7 +15,6 @@ app.use(
 );
 app.use("/", proNumbers);
 app.use("/", consigneeRoutes);
-// app.use("/", sqlConnector);
 app.use(express.static("public"));
 
 app.listen(port, () => {
--- backend/routes/consigneeRoutes.js
+++ backend/routes/consigneeRoutes.js
@@ -12,22 +12,16 @@ const dummyJSON = {
     "UNFI - Moreno Valley",
   ],
   avgTransitTimes: [3, 2, 3, 1, 7],
-  shipFrom: ["DUR", "SF", "DUR", "DUR", "SF"],
 };
 
 /**
  * Router to define all consignee routes
- * @param {Router} router
+ * @param {router} router
  *
  */
-// export const consigneeRoutes = (router) => {
-// defining the route
-// localhost:3000/get-consignees
+
 router.get("/get-consignees", (req, res) => {
   res.json(dummyJSON);
-  // also go to MySQL and execute a query
   // let data = getDataFromSQL()
-  // res.json(data)
 });
 module.exports = router;
-// };
